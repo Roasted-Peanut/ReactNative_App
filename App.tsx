6bacca495e6832187267e7d45183221b70109193
PhungPV <vanphung0198@gmail.com>
2024-03-06 10:05:40 +0700

    commit prettier

--- App.tsx
+++ App.tsx
@@ -6,19 +6,21 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import MainLayout from "./src/MainLayout";
 import { DownloadMediaModule, TestNativeModule } from "./src/native_module";
-import {  store, persistor } from "./src/redux/store";
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { store, persistor } from "./src/redux/store";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 import { useGetProductListQuery } from "./src/graphql/queries/__generated__/graphql";
 
 const client = new ApolloClient({
-  uri: 'http://115.146.127.172:8185/shop-api/',
+  uri: "http://115.146.127.172:8185/shop-api/",
   cache: new InMemoryCache(),
 });
 
 function App(): React.JSX.Element {
-
-
-
   useEffect(() => {
     if (Platform.OS == "android") {
       const downloadMedia = DownloadMediaModule;
@@ -31,13 +33,13 @@ function App(): React.JSX.Element {
 
   return (
     <ApolloProvider client={client}>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <MainLayout />
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <SafeAreaProvider>
+            <MainLayout />
+          </SafeAreaProvider>
+        </PersistGate>
+      </Provider>
     </ApolloProvider>
   );
 }
